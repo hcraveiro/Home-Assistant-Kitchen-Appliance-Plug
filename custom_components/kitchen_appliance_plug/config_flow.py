5196096b7805ac99c64d6a5e032760e7efe7cda2
hcraveiro <henrique.craveiro@gmail.com>
2026-02-03 12:07:13 +0000

    - Fixed HTTP 500 issue with Config Flow

--- custom_components/kitchen_appliance_plug/config_flow.py
+++ custom_components/kitchen_appliance_plug/config_flow.py
@@ -69,7 +69,7 @@ class KitchenAppliancePlugConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
 class KitchenAppliancePlugOptionsFlowHandler(config_entries.OptionsFlow):
     def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
-        self.config_entry = config_entry
+        self._config_entry = config_entry
 
     async def async_step_init(self, user_input=None):
         if user_input is not None:
